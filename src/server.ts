e9d7aa3cd7081242f3e3d0c8ac39207e654387e4
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-12-21 17:42:54 -0300

    feat: use router file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express from "express";
 
-// import routes from './routes/index.routes';
+import routes from "./routes/index";
 
 const port = 3333;
 const app = express();
 
 app.use(express.json());
-// app.use(routes);
-app.get("/", (req, res) => {
-  res.send("Welcome to transport API!");
-});
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
