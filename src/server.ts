f1b321e69c23ef3640405e9084f67147872e164c
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-12-21 15:33:27 -0300

    fix: typo

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 // app.use(routes);
 app.get("/", (req, res) => {
-  res.send("Welcome to transportation API!");
+  res.send("Welcome to transport API!");
 });
 
 app.listen(port, () => {
