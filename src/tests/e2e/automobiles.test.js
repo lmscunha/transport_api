3e529db31199d60a316706f1e0b95063e8a4fb1f
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-12-21 20:09:31 -0300

    test: create automobiles route

---
+++ src/tests/e2e/automobiles.test.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+const { app, server } = require("../../server");
+
+describe("Automobiles Resource", () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  describe("POST /automobiles", () => {
+    test("should register automobile with license plate ABC", async () => {
+      const res = await request(app)
+        .post("/automobiles")
+        .send({
+          automobile: {
+            licensePlate: "ABC",
+            color: "Green",
+            brand: "Foo",
+          },
+        });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.result.licensePlate).toEqual("ABC");
+      expect(res.body.result.color).toEqual("Green");
+      expect(res.body.result.brand).toEqual("Foo");
+    });
+
+    test("should register automobile with license plate XYZ", async () => {
+      const res = await request(app)
+        .post("/automobiles")
+        .send({
+          automobile: {
+            licensePlate: "XYZ",
+            color: "Blue",
+            brand: "Baa",
+          },
+        });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.result.licensePlate).toEqual("XYZ");
+      expect(res.body.result.color).toEqual("Blue");
+      expect(res.body.result.brand).toEqual("Baa");
+    });
+  });
+});
