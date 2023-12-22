76633eb457cbf9ab9fa47a7e2fc4866e91c68472
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-12-22 19:43:18 -0300

    test: create route to list all automobiles

--- src/modules/automobiles/tests/e2e/automobiles.test.js
+++ src/modules/automobiles/tests/e2e/automobiles.test.js
@@ -6,37 +6,41 @@ describe("Automobiles Resource", () => {
     server.close();
   });
 
-  describe("POST /automobiles", () => {
-    test("should register automobile with license plate ABC", async () => {
-      const res = await request(app).post("/automobiles").send({
-        licensePlate: "ABC",
-        color: "Green",
+  describe("GET /automobiles", () => {
+    test("should list all automobiles", async () => {
+      const autoCreated1 = await request(app).post("/automobiles").send({
+        licensePlate: "ABC1",
+        color: "Red",
         brand: "Foo",
       });
 
-      expect(res.statusCode).toBe(201);
-      expect(res.body.automobile).toHaveProperty("id");
-      expect(res.body.automobile.licensePlate).toEqual("ABC");
-      expect(res.body.automobile.color).toEqual("Green");
-      expect(res.body.automobile.brand).toEqual("Foo");
-    });
-
-    test("should register automobile with license plate XYZ", async () => {
-      const res = await request(app).post("/automobiles").send({
-        licensePlate: "XYZ",
+      const autoCreated2 = await request(app).post("/automobiles").send({
+        licensePlate: "ABC2",
         color: "Blue",
         brand: "Baa",
       });
 
-      expect(res.statusCode).toBe(201);
-      expect(res.body.automobile).toHaveProperty("id");
-      expect(res.body.automobile.licensePlate).toEqual("XYZ");
-      expect(res.body.automobile.color).toEqual("Blue");
-      expect(res.body.automobile.brand).toEqual("Baa");
-    });
-  });
+      const autoCreated3 = await request(app).post("/automobiles").send({
+        licensePlate: "ABC3",
+        color: "Yellow",
+        brand: "Foo Baa",
+      });
 
-  describe("GET by ID /automobiles", () => {
+      const res = await request(app).get(`/automobiles`);
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.automobiles.length).toEqual(3);
+      expect(res.body.automobiles[0].id).toEqual(
+        autoCreated1.body.automobile.id,
+      );
+      expect(res.body.automobiles[1].id).toEqual(
+        autoCreated2.body.automobile.id,
+      );
+      expect(res.body.automobiles[2].id).toEqual(
+        autoCreated3.body.automobile.id,
+      );
+    });
+
     test("should get the automobile with license plate ABC by ID", async () => {
       const autoCreated = await request(app).post("/automobiles").send({
         licensePlate: "ABC",
@@ -69,6 +73,36 @@ describe("Automobiles Resource", () => {
       expect(res.body.automobile.licensePlate).toEqual("XYZ");
       expect(res.body.automobile.color).toEqual("Yellow");
       expect(res.body.automobile.brand).toEqual("Foo");
+    });
+  });
+
+  describe("POST /automobiles", () => {
+    test("should register automobile with license plate ABC", async () => {
+      const res = await request(app).post("/automobiles").send({
+        licensePlate: "ABC",
+        color: "Green",
+        brand: "Foo",
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.automobile).toHaveProperty("id");
+      expect(res.body.automobile.licensePlate).toEqual("ABC");
+      expect(res.body.automobile.color).toEqual("Green");
+      expect(res.body.automobile.brand).toEqual("Foo");
+    });
+
+    test("should register automobile with license plate XYZ", async () => {
+      const res = await request(app).post("/automobiles").send({
+        licensePlate: "XYZ",
+        color: "Blue",
+        brand: "Baa",
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.automobile).toHaveProperty("id");
+      expect(res.body.automobile.licensePlate).toEqual("XYZ");
+      expect(res.body.automobile.color).toEqual("Blue");
+      expect(res.body.automobile.brand).toEqual("Baa");
     });
   });
 
