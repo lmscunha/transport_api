44d49b0608ddbcd960d17d7f648520aae7e7f2e3
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-12-23 20:29:38 -0300

    feat: update auto domain

--- src/modules/automobiles/domain/automobile-protocols.ts
+++ src/modules/automobiles/domain/automobile-protocols.ts
@@ -5,6 +5,7 @@ export interface AutomobileProvider {
   getAll: () => Promise<Automobile[] | []>;
   filterBy: (params: FilterDTO) => Promise<Automobile[] | []>;
   getById: (id: string) => Promise<Automobile>;
+  isValidPlate: (licensePlate: string) => Promise<boolean>;
   save: (automobile: StorageDTO) => Promise<Automobile>;
   update: (id: string, automobile: InputDTO) => Promise<Automobile | null>;
   delete: (id: string) => Promise<void>;
--- src/modules/automobiles/domain/automobile-use-cases.ts
+++ src/modules/automobiles/domain/automobile-use-cases.ts
@@ -1,10 +1,9 @@
-import { Automobile } from "./AutomobileModel";
-import { InputDTO, FilterDTO } from "./automobile-dtos";
+import { InputDTO, FilterDTO, ResultDTO } from "./automobile-dtos";
 
 export interface AutomobileUseCases {
-  getAllAutomobiles: (params: FilterDTO) => Promise<Automobile[] | []>;
-  getAnAutomobile: (id: string) => Promise<Automobile | null>;
-  registerAutomobile: (autombile: InputDTO) => Promise<Automobile>;
-  updateAutomobile: (id: string, automobile: InputDTO) => Promise<Automobile>;
-  deleteAutomobile: (id: string) => Promise<void>;
+  getAllAutomobiles: (params: FilterDTO) => Promise<ResultDTO | []>;
+  getAnAutomobile: (id: string) => Promise<ResultDTO>;
+  registerAutomobile: (autombile: InputDTO) => Promise<ResultDTO>;
+  updateAutomobile: (id: string, automobile: InputDTO) => Promise<ResultDTO>;
+  deleteAutomobile: (id: string) => Promise<void | ResultDTO>;
 }
